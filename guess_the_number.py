71b928e54dca24d2f6ddc4b467a078c596544015
Shubham Srivastava <shubham2907srivastava@gmail.com>
2020-12-12 15:01:22 +0530

    Python code for Computer guess

--- guess_the_number.py
+++ guess_the_number.py
@@ -16,5 +16,22 @@ def guess_user(x):
     print("Yay congrats !! You guessed right answer ")
 
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    guess = 0
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f"Is the number {guess} ?")
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"Yay, The computer guessed your number {guess} correctly !!")
+
+
 if __name__ == "__main__":
-    guess_user(10)
+    # guess_user(10)
+    computer_guess(10)
